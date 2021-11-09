5b0f77061751c830f0fcf9f73fb1d055e02bb5cc
Naman065Malik <86320160+Naman065Malik@users.noreply.github.com>
2021-11-09 22:32:52 +0530

    Add files via upload

---
+++ Day 5 Assignment 3.py
@@ -0,0 +1,46 @@
+"""
+Assignment 3 (calculator)
+Make a calculator using function along with if, elif and else statement.
+
+Addition , Subtraction , Multiplication , Division
+
+You enter two numbers and operator you want to do, it will return the answer.
+"""
+
+from typing import AnyStr
+
+
+def Add(num1,num2):
+    ans = num1 + num2
+    return ans
+
+def Sub(num1,num2):
+    ans = num1 - num2
+    return ans
+
+def Mul(num1,num2):
+    ans = num1 * num2
+    return ans
+
+def Div(num1,num2):
+    ans = num1 / num2
+    return ans
+
+opr = input("Please enter your operation: ")
+opr = opr.lower()
+num1 = float(input("Please enter your first number: "))
+num2 = float(input("Please enter your second number: "))
+if opr == 'addition':
+    ans = Add(num1,num2)
+    print("Your answer is",ans)
+elif opr == "subtraction":
+    ans = Sub(num1,num2)
+    print("Your answer is",ans)
+elif opr == "multiplication":
+    ans = Mul(num1,num2)
+    print("Your answer is",ans)
+elif opr == "division":
+    ans = Div(num1,num2)
+    print("Your answer is",ans)
+else:
+    print("Please enter a correct operation")      
